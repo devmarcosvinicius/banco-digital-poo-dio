a9f585eb482e80d0f1dd796f1cee07ae6056debf
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-12-03 00:17:36 -0300

    feat: validate methods sacar, depositar, transferir e implementando o metodo imprimirInfosComuns

--- src/com/marcosviniciusdev/Conta.java
+++ src/com/marcosviniciusdev/Conta.java
@@ -1,6 +1,6 @@
 package com.marcosviniciusdev;
 
-public class Conta implements iConta {
+public abstract class Conta implements iConta {
 
     protected static final int AGENCIA_PADRAO = 1;
     private static int SEQUENCIAL = 1;
@@ -29,16 +29,51 @@ public class Conta implements iConta {
 
     @Override
     public void sacar(double valor) {
+        if (valor <= 0) {
+            System.out.println("O valor de saque deve ser maior que zero.");
+            return;
+        }
 
+        if (valor > this.getSaldo()) {
+            System.out.println("O valor de saque é maior que o seu saldo.");
+            return;
+        }
+
+        this.saldo += valor;
     }
 
     @Override
     public void depositar(double valor) {
+        if (valor <= 0) {
+            System.out.println("Valor de deposito deve ser maior que zero.");
+            return;
+        }
 
+        this.saldo += valor;
     }
 
     @Override
     public void transferir(Conta contaDestino, double valor) {
+        if (contaDestino == null) {
+            System.out.println("Conta invalida.");
+            return;
+        }
+
+        if (valor <= 0) {
+            System.out.println("O valor de transferencia é invalido.");
+            return;
+        }
+
+        this.saldo -= valor;
+        contaDestino.depositar(valor);
+        System.out.println("Transferencia de R$" + valor + " feita com sucesso.");
 
     }
+
+    public void imprimirInfosComuns() {
+        System.out.printf("Agencia: %d%n", this.agencia);
+        System.out.printf("Conta: %d%n", this.numero);
+        System.out.printf("Saldo: %.2f%n", this.saldo);
+    }
+
 }
