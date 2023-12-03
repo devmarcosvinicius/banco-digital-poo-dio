3413c8740300ea12921f3516e6fa74b3b2180f08
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-12-03 00:38:58 -0300

    feat: create attributes and getter/setter

---
+++ src/com/marcosviniciusdev/ContaCorrente.java
@@ -0,0 +1,14 @@
+package com.marcosviniciusdev;
+
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("\n=-=-= Extrato Conta Corrente =-=-=");
+        super.imprimirInfosComuns();
+    }
+}
