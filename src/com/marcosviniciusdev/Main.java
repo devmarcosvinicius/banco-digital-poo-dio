5e66b8de67732afb39a35ce5a6e9830c35d9ee03
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-12-03 13:26:51 -0300

    test: add println to show Clientes in Conta.

--- src/com/marcosviniciusdev/Main.java
+++ src/com/marcosviniciusdev/Main.java
@@ -3,8 +3,8 @@ package com.marcosviniciusdev;
 public class Main {
 
     public static void main(String[] args) {
-        Cliente c1 = new Cliente("Marcos");
-        Cliente c2 = new Cliente("Fernanda");
+        Cliente c1 = new Cliente("Marcos", "123456789-00", "10/07/1998");
+        Cliente c2 = new Cliente("Fernanda", "123456789-01", "05/12/1986");
 
         Conta cc = new ContaCorrente(c1);
         Conta poupanca = new ContaPoupanca(c2);
@@ -13,6 +13,9 @@ public class Main {
 
         cc.imprimirExtrato();
         poupanca.imprimirExtrato();
+
+        System.out.println(cc.getCliente().getNome());
+        System.out.println(poupanca.getCliente().getNome());
 
     }
 
