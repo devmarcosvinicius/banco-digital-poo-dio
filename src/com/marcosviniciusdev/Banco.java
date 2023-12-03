2fa5339e84bbd73d155e600f0e4120147aca9e08
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-12-03 01:20:04 -0300

    refactor: remove getters/setters and add @Data lombok annotation.

--- src/com/marcosviniciusdev/Banco.java
+++ src/com/marcosviniciusdev/Banco.java
@@ -1,25 +1,11 @@
 package com.marcosviniciusdev;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class Banco {
-
     private String nome;
     private List<Conta> contas;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<Conta> getContas() {
-        return contas;
-    }
-
-    public void setContas(List<Conta> contas) {
-        this.contas = contas;
-    }
 }
