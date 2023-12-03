a19bf9771ace621dfaba78dbe387e6d9f0bb3088
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-12-03 01:21:17 -0300

    refactor: remove getters/setters, constructor and add @Data and @AllArgsConstructor lombok annotations.

--- src/com/marcosviniciusdev/Cliente.java
+++ src/com/marcosviniciusdev/Cliente.java
@@ -1,18 +1,10 @@
 package com.marcosviniciusdev;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Cliente {
-
     private String nome;
-
-    public Cliente(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
